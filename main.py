723b9cd23ee5bff12a274681f9025d786d96d7e5
Jonas Bucher <j.bucher.mn@gmail.com>
2019-01-05 12:41:23 +0100

    Parameter values appear to be valid; setting works

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import serial
 import glob
 import csv
+from crccheck.crc import CrcX25
 
 
 options = []
@@ -12,23 +13,123 @@ with open('options.csv', 'r') as opts:
         options += [r]
 
 
+class Parameter:
+    def __init__(self, type, name, options):
+        self.type = type
+        self.name = name
+        self.options = options
+
+        self.value = None
+
+    def read(self, param_bytes):
+        addr = int(self.options['addr']) * 2
+        sub = [param_bytes[addr+1], param_bytes[addr]]
+
+        self.value = self._parse(sub)
+
+    def set(self, param_bytes):
+        value = self._encode()
+        value[0], value[1] = value[1], value[0]
+        addr = int(self.options['addr']) * 2
+
+        param_bytes[addr:addr+2] = value
+
+
+class SIParameter(Parameter):
+    def __init__(self, options):
+        super().__init__("SI", options['name'], options)
+
+    def _parse(self, sub):
+        v = 256 * sub[0] + sub[1]
+        if v > 32767:
+            v -= 65536
+
+        return v
+
+    def _encode(self):
+        if self.value < 0:
+            self.value += 65536
+
+        return [int(self.value/256), self.value % 256]
+
+
+class UIParameter(Parameter):
+    def __init__(self, options):
+        super().__init__("UI", options['name'], options)
+
+    def _parse(self, sub):
+        return 256 * sub[0] + sub[1]
+
+    def _encode(self):
+        return [int(self.value/256), self.value % 256]
+
+
+class ListParameter(Parameter):
+    def __init__(self, options):
+        super().__init__("LISTA", options['name'], options)
+
+    def _parse(self, sub):
+        return 256 * sub[0] + sub[1]
+
+    def _encode(self):
+        return [int(self.value/256), self.value % 256]
+
+
+def parameter_factory(option):
+    if option['type'] in ['SCRIPT', 'STR']:
+        return None
+    elif option['type'] == 'SI':
+        return SIParameter(option)
+    elif option['type'] == 'UI':
+        return UIParameter(option)
+    elif option['type'] == 'LISTA':
+        return ListParameter(option)
+    else:
+        raise Exception("Unknown type %s" % option['type'])
+
+
+def checksum(bytelike):
+    crc = 0xFFFF
+    for b in bytelike:
+        tmp = b ^ (crc & 0xFF)
+        tmp = (tmp ^ (tmp << 4)) & 0xFF
+        crc = (crc >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)
+        crc = crc & 0xFFFF
+
+    return bytes([crc % 256, int(crc/256)])
+
+
 class STorM32:
     def __init__(self, device, baud):
         self._ser = serial.Serial(device, baud, timeout=1)
+        self._param_bytes = None
+        self._param_bytes_changed = bytearray()
+        self._parameters = []
+        for o in options:
+            p = parameter_factory(o)
+            if p is not None:
+                self._parameters += [p]
+
         self.ping()
 
-    def _read_answer(self):
-        answer = b'\n'.join(self._ser.readlines())
+    def _read_answer(self, answer_size=-1):
+        """
+        answer_size DOES include o/e status; returned answer does NOT
+        """
+        if answer_size == -1:
+            answer = b''.join(self._ser.readlines())
+        else:
+            answer = self._ser.read(answer_size)
         if answer[-1] == b'e':
             raise Exception("STorM32 returned error code")
         elif answer[-1:] != b'o':
-            raise Exception("STorM32 returned bogus")
+            raise Exception("STorM32 returned bogus: %s" % answer)
         else:
             return answer[:-1]
 
     def ping(self):
         self._ser.write(b't')
-        if self._read_answer() != b'':
+        if self._read_answer(1) != b'':
             raise Exception("Ping failed")
 
     def get_version(self):
@@ -39,36 +140,46 @@ class STorM32:
     def get_parameters(self):
         self.ping()
         self._ser.write(b'g')
-        params = self._read_answer()
+        self._param_bytes = self._read_answer()
+        crc = self._param_bytes[-2:]
+        self._param_bytes = self._param_bytes[:-2]
 
-        result = []
-        for o in options:
-            if o['type'] == 'SCRIPT':
-                continue
+        print(checksum(self._param_bytes), crc)
 
-            addr = int(o['addr']) * 2
-            sub = [params[addr+1], params[addr]]
+        for p in self._parameters:
+            p.read(self._param_bytes)
 
-            v = 255 * sub[0] + sub[1]
-            if o['type'] == 'SI' and v > 32767:
-                v -= 65536
-            elif o['type'] == 'SC' and v > 127:
-                v -= 256
+    def set_parameters(self):
+        self._param_bytes_changed = bytearray(self._param_bytes)
+        for p in self._parameters:
+            p.set(self._param_bytes_changed)
 
-            print("%30s, %8d, %8d, %8d" % (o['name'], int(o['min']), v, int(o['max'])))
+        params = b'p' + bytes(self._param_bytes_changed) + \
+            checksum(self._param_bytes_changed)
 
-            v = 1
-            result += [{
-                'option': o,
-                'value': v
-            }]
+        print("Writing parameters...")
+        self._ser.write(params)
+        try:
+            self._read_answer(answer_size=1)
+            print("...success")
+        except Exception:
+            print("....failed")
 
-        return result
+    def print_parameters(self):
+        for p in self._parameters:
+            print("%30s: %d" % (p.name, p.value))
+
+    # def get_live_data(self):
+    #     self._ser.write(b'd')
+    #     result = self._read_answer()
+    #     crc = result[-2:]
+    #     result = result[:-2]
 
 
 for tty in glob.glob('/dev/tty.usb*'):
     print("Opening: %s" % tty)
     st = STorM32(tty, 9600)
     st.ping()
-    print("Version: %s" % st.get_version())
     st.get_parameters()
+    st.print_parameters()
+    st.set_parameters()
